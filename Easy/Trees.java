2538c0a5dd3cabea7819e070b159afb20c5b7692
sarthak <sarthak.shinde2006@gmail.com>
2025-12-10 11:16:13 +0530

    Level_Order_Traversal

--- Easy/Trees.java
+++ Easy/Trees.java
@@ -25,16 +25,47 @@ public class Trees{
             return newNode;
             
         }
+    }
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
 
-
+    public static void postorder(Node root){
+         if(root==null){
+            return;
+        }
+         
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data+" ");
+    }
+    public static void inorder(Node root){
+         if(root==null){
+            return;
+        }
+        postorder(root.left);
+         System.out.print(root.data+" ");
+        postorder(root.right);
+       
 
     }
+
 
     public static void main(String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
         Node root=tree.BuildTree(nodes);
         System.out.println(root.data);
+        preorder(root);
+        System.out.println();
+        postorder(root);
+        System.out.println();
+        inorder(root);
 
 
     }
